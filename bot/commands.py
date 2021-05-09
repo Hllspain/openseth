b8a41139a61b373403f9e9ce8486182b9497647b
TurBoss <j.l.toledano.l@gmail.com>
2021-05-09 08:30:47 +0200

    command to get members that reacted to a message

--- bot/commands.py
+++ bot/commands.py
@@ -1,8 +1,11 @@
 import asyncio
 import random
+from pprint import pprint
 
 import discord
+from discord import NotFound
 from discord.ext import commands
+from discord.utils import get
 
 from yaml import load
 from yaml import Loader
@@ -50,13 +53,42 @@ async def spam_role(ctx: commands.Context, target: discord.Role, message: str):
     await ctx.send(f"Yes Sir")
     for user in ctx.guild.members:
         if target in user.roles:
-            welcome = f"hola {user.name}"
-            await send_user_dm(user, welcome)
+            # welcome = f"hola {user.name}"
+            # await send_user_dm(user, welcome)
             await send_user_dm(user, message)
             await asyncio.sleep(3)
             num_users += 1
             # await asyncio.sleep(random.choice([40, 60]))
 
     await ctx.send(f"Done spam sent to {num_users} users")
+
+
+@bot.command(pass_context=True)
+@commands.has_role("Administrador")
+async def get_members(ctx, channel: discord.TextChannel, message_id: int, role: discord.Role):
+    try:
+        msg = await channel.fetch_message(message_id)
+
+        pprint(msg)
+
+        reactions = msg.reactions
+
+        print(reactions)
+
+        for reactor in reactions:
+            if str(reactor) == '👍':
+                print(reactor)
+                print("users")
+
+                async for user in reactor.users():
+                    pass
+                    # await user.add_roles(role)
+
+            # reactors = await client(reactor)
+            # #from here you can do whatever you need with the member objects
+            # for member in reactors
+            #     print(member.name)
+    except NotFound:
+        print("not found")
 
 bot.run(discord_token)
