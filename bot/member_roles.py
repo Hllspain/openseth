64ed8784f035b07867892c45e71ec3e78ac735db
TurBoss <j.l.toledano.l@gmail.com>
2021-11-24 03:57:49 +0100

    add roles to members that accepted rules

---
+++ bot/member_roles.py
@@ -0,0 +1,46 @@
+
+import time
+import discord
+from discord.utils import get
+
+from yaml import load
+from yaml import Loader
+
+with open("config.yml") as f:
+    config = load(f.read(), Loader=Loader)
+
+discord_token = config["bot"]["discord_token"]
+guild_id = config["bot"]["guild"]
+roles_id = config["bot"]["roles"]
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
+
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_member_update(before, after):
+    if before.pending == True and after.pending == False:
+        
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        print(f"{current_time} Adding {after} new role")
+        
+        guild = client.get_guild(guild_id)
+        for role_id in roles_id:
+            role = get(guild.roles, id=role_id)
+        
+            if role is not None:
+                print(f"\t{role}")
+                await after.add_roles(role)
+            
+
+client.run(discord_token)
