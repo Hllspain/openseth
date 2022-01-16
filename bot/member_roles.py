11d64b7fc9bd1587031cb45bab8bff678448eb61
TurBoss <j.l.toledano.l@gmail.com>
2022-01-17 00:01:32 +0100

    minor tweaks

--- bot/member_roles.py
+++ bot/member_roles.py
@@ -41,14 +41,14 @@ async def on_member_update(before, after):
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         
-        await channel.send(f"{current_time} Adding {after} <@{after.id}> new role")
+        await channel.send(f"{current_time} Nuevo membro {after} <@{after.id}>")
         
         for role_id in roles_id:
             role = get(guild.roles, id=role_id)
         
             if role is not None:
                 await channel.send(f"\t<@{role_id}>")
-                await after.add_roles(role)
+                # await after.add_roles(role)
             
         await after.send(welcome_msg)
 
