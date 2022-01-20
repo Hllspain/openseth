07f8388257abd6e880579b6ea49147a7acf7e034
TurBoss <j.l.toledano.l@gmail.com>
2022-01-20 22:16:08 +0100

    add nice arrows

--- bot/member_roles.py
+++ bot/member_roles.py
@@ -36,7 +36,7 @@ async def on_member_remove(member):
         
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
-    await channel.send(f"{current_time} Membro <@{member.id}> {member} se fue.")
+    await channel.send(f"{current_time} <-- Membro <@{member.id}> {member} se fue.")
 
 @client.event
 async def on_member_update(before, after):
@@ -49,7 +49,7 @@ async def on_member_update(before, after):
         t = time.localtime()
         current_time = time.strftime("%H:%M:%S", t)
         
-        await channel.send(f"{current_time} Nuevo miembro <@{after.id}>")
+        await channel.send(f"{current_time} --> Nuevo miembro <@{after.id}>")
         
         for role_id in roles_id:
             role = get(guild.roles, id=role_id)
